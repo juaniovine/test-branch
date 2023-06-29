7f26d752faa778dcd31d72f40c8966cec3447bc2
juaniovine <juaniovine@gmail.com>
2023-06-29 19:00:32 -0300

    Add js file

---
+++ index.js
@@ -0,0 +1,10 @@
+let nombreUsuario = prompt("Ingrese su nombre")
+
+console.log (nombreUsuario)
+
+// let nombreUsuario1 = 'Su nombre es Pirulo'
+
+// console.log(nombreUsuario1)
+
+alert("Hola" + ' ' + nombreUsuario)
+ 
