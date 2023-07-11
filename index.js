8ff81884852adb466cf6af6be70ae1ea76f3fa3d
juaniovine <juaniovine@gmail.com>
2023-07-11 14:41:22 -0300

    add pruebas js clase

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-let nombreUsuario = prompt("Ingrese su nombre")
+// let nombreUsuario = prompt("Ingrese su nombre")
 
-console.log (nombreUsuario)
+// console.log (nombreUsuario)
 
 // let nombreUsuario1 = 'Su nombre es Pirulo'
 
 // console.log(nombreUsuario1)
 
-alert("Hola" + ' ' + nombreUsuario)
+// alert("Hola" + ' ' + nombreUsuario)
  
+
+// Ejercicio 1:
+// Escribe un script que solicite al usuario un número e imprima en la consola si ese número es positivo, negativo o cero.
+
+// const number = prompt("Ingrese un Numero")
+
+// if ( number == 0 ){
+//     console.log("Usted ingreso el Numero " + number)
+// } else if ( number < 0 ) {
+//     console.log("Usted ingreso un Numero negativo")
+// } else if ( number > 0 ) {
+//     console.log("Usted ingreso un Numero positivo")
+// } else {
+//     console.log("Usted no ingreso un Numero Valido")
+// }
+
+
+// Ejercicio 2:
+// Escribe un script que solicite al usuario dos números e imprima en la consola el número mayor de los dos.
+
+const number01 = prompt("Ingrese el primer Numero");
+const number02 = prompt("Ingrese el swegundo Numero");
+
+if ( number01 > number02 ){
+    console.log("El número mayor es " + number01)
+} else if ( number01 < number02 ) {
+    console.log("El número mayor es " + number02)
+} else if ( number01 == number02 ) {
+    console.log("Usted ingreso valores iguales")
+} else {
+    console.log("Usted no ingreso un Numero Valido")
+}
+
+
+// Ejercicio 3:
+// Escribe un script que solicite al usuario su edad e imprima en la consola si es mayor de edad o no (considerando 18 años como la mayoría de edad).
+
+
+
+// Ejercicio 4:
+// Escribe un script que solicite al usuario un número e imprima en la consola si ese número es par o impar.
+
+
+
+// Ejercicio 5:
+// Escribe un script que solicite al usuario tres números e imprima en la consola el número más grande de los tres.
+
+
